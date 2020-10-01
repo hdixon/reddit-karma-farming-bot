e9967c3b6aa878a5e909b25d30c46b6b8a9dcbba
Henry Dixon <gogogadgethd@gmail.com>
2020-10-01 00:01:48 -0400

    Update settings.sample.py

--- src/settings.sample.py
+++ src/settings.sample.py
@@ -1,6 +1,6 @@
 # Reddit API
-REDDIT_CLIENT_ID="MyCoolClientId"
-REDDIT_SECRET="MyCoolSecret"
-REDDIT_USERNAME="MyCoolUsername"
-REDDIT_PASSWORD="MyCoolPassword"
-REDDIT_USER_AGENT="script by /u/" # This can be anything you want.
+REDDIT_CLIENT_ID="76a3ZDRbL4f4MDcicItyVCDA4Vc"
+REDDIT_SECRET="ZvEfyzizfH_cPA"
+REDDIT_USERNAME="TraditionalSpinach40"
+REDDIT_PASSWORD="nuehok2901"
+REDDIT_USER_AGENT="script by u/bullcrack" # This can be anything you want.
